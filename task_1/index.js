e06c16c6a309d367aca0353c6b7a3bdfc4852291
Denis <d.m.2005qqq@gmail.com>
2024-04-23 15:15:25 +0200

    Task 1 done

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,32 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+const uniqueIds = new Set();
 
+const decoded = [];
 
-// console.log(decoded)
+encoded.forEach(obj => {
+  for (const key in obj) {
+    if (key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+      const id = obj[key];
+      if (id !== null) {
+        uniqueIds.add(id);
+      }
+    }
+  }
+
+  const newObj = { ...obj };
+  for (const key in newObj) {
+    if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+      const id = newObj[key];
+      if (id !== null) {
+        newObj[key] = translations[id];
+      }
+    }
+  }
+
+  decoded.push(newObj);
+});
+
+console.log(decoded)
+console.log(uniqueIds);
